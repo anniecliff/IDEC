475419f2a0e1eca90e712eade960a65f423a5387
Annie <anniealencliff@gmail.com>
2017-11-03 17:01:54 +0530

    Add files via upload

---
+++ event_view.php
@@ -0,0 +1,121 @@
+<?php include "header.html"; ?>
+
+
+   <div class="hide-on-large-only">
+      <section class="gradient">
+         <div class="row">
+            <div class="col s12">
+           
+               <div class="page-header center-align">
+                  <img class="circle center-img" src="Img/Eventphoto-10.png">
+                  <h5 class="white-text" style="margin: 0.82rem 0 1.656rem 0 !important;">Acceler8 2nd Anniversary</h5>
+                    <div class="col s6 center">
+                <table style="margin-left: 77px;">
+                     <tr class="tr" style="border-style: solid !important;border-width: 1px !important;">
+                        <td class="profileedit">
+
+		                        <p style="margin-top: 13px;font-size: smaller; padding-left: 30px;">RSVP Event</p>
+                        </td>
+                     </tr>
+                  </table>
+                </div>
+           
+               </div>
+
+            </div>
+         </div>
+      </section>
+      <section>
+         <div class="container">
+            <div class="row">
+               <div class="col s12 page-header">
+                        <div class="row">
+               <div class="col s12">
+                  <div class="input-field col s12 event">
+                             
+                      <input type="text" value="WE know you've been waiting for this. Check out this jam packed program we have for you.This massive lineup is all FREE. See you there on thursday ! ">
+                <label for="user_name" class="event" >Event description</label>
+                                 </div>
+               </div>
+               <div class="col s12">
+                 <div class="input-field col s12 location">
+                             
+                                    <input  type="text" value="Paseo de roxas Bldg,Makati city">
+                                    <label for="user_name" class="location">Location</label>
+                                 </div>
+               </div>
+               <div class="col s12">
+                  <div class="input-field col s12 date">
+                             
+                                    <input  type="text" value="November 22,2017 6:00PM">
+                                    <label for="user_name" class="date">Date</label>
+                                 </div>
+               </div>
+
+                                 </div>
+            </div>
+         </div>
+      </section>
+   </div>
+
+ <div class="hide-on-med-and-down">
+      <section class="gradient">
+        <div class="container">
+         <div class="row">
+            <div class="col l12">
+           
+               <div class="page-header center-align">
+                  <img class="circle center-img" src="Img/Eventphoto-10.png" style="width: 20% !important;">
+                  <h5 class="white-text" style="margin: 0.82rem 0 1.656rem 0 !important;">Acceler8 2nd Anniversary</h5>
+                    <div class="col s6 center">
+                <table style="margin-left: 250px;">
+                     <tr class="tr" style="border-style: solid !important;border-width: 1px !important;">
+                        <td class="profileedit">
+
+		                        <p style="margin-top: 13px;font-size: large;padding-left: 218px;">RSVP Event</p>
+                        </td>
+                     </tr>
+                  </table>
+                </div>
+           
+               </div>
+
+            </div>
+         </div>
+         </div>
+      </section>
+      <section>
+         <div class="container">
+            <div class="row">
+      <div class="col s12 page-header center-align">
+              <div class="col s12">
+                  <div class="input-field col l12 event">
+                             
+                      <input type="text" value="WE know you've been waiting for this. Check out this jam packed program we have for you.This massive lineup is all FREE. See you there on thursday ! ">
+                <label for="user_name" class="event" >Event description</label>
+                                 </div>
+               </div>
+               <div class="col s12">
+                 <div class="input-field col l12 location">
+                             
+                                    <input  type="text" value="Paseo de roxas Bldg,Makati city">
+                                    <label for="user_name" class="location">Location</label>
+                                 </div>
+               </div>
+               <div class="col s12">
+                  <div class="input-field col l12 date">
+                             
+                                    <input  type="text" value="November 22,2017 6:00PM">
+                                    <label for="user_name" class="date">Date</label>
+                                 </div>
+               </div>
+                                 </div>
+                                 </div>
+         </div>
+      </section>
+   </div>
+   
+<?php include "footer.html"; ?>
+
+
+  
---
+++ events.php
@@ -0,0 +1,477 @@
+<?php include "header.html"; ?>
+<style type="text/css">
+.company{
+margin-top: 3rem !important;
+
+
+}
+hr.mylist {
+	/* border: none; */
+border-top: 1px solid #a59f9fb3 !important;
+	margin-right: 0px !important;
+	margin-top: 164px !important;
+}
+.search
+{
+	display: flex !important;
+}
+@media only screen and (min-width : 992px) {
+   .search {
+      display: none !important;
+   }
+   }
+</style>
+   <div class="hide-on-large-only">
+      
+      <nav class="gradients" style="height: 64px !important;">
+     
+         <div class="nav-wrapper" style="height: 64px !important;" >
+        <ul id="tabs-swipe-demo" class="tabs">
+    <li class="tab col s3"><a  class="active" href="#test-swipe-1" style="color:white;">ALL</a></li>
+    <li class="tab col s3"><a   href="#test-swipe-2" style="color:white;">UPCOMING</a></li>
+    <li class="tab col s3"><a  href="#test-swipe-3" style="color:white;">PREVIOUS</a></li>
+  </ul>
+ 
+           
+         </div>
+      </nav>
+   
+       <div id="test-swipe-1" class="col s12">
+       
+		<div class="col s12 m7">
+		<a href="#" style="color:black">
+				<div class="card horizontal" style="margin: 1.5rem 15px 3rem 12px;">
+      			<div class="card-image" style="max-width: 30%;">
+        				<img src="Img/Eventphoto-10.png" >
+      			</div>
+      			<div class="card-stacked">
+        				<div class="card-content">
+ 										 <p style="font-size: calc;font-weight: bolder;">Acceler8 2nd Anniversary </p>
+		              			    <p class="place">Coworking community</p>
+		              			    <p class="place">November22,2017 6:00 PM </p>
+        				</div>
+        				
+      			</div>
+    			</div>
+    			</a>
+ 			</div>
+<div class="col s12 m7">
+		<a href="#" style="color:black">
+				<div class="card horizontal" style="margin: 1.5rem 15px 3rem 12px;">
+      			<div class="card-image" style="max-width: 30%;">
+        				<img src="Img/Eventphoto-10.png" >
+      			</div>
+      			<div class="card-stacked">
+        				<div class="card-content">
+ 										 <p style="font-size: calc;font-weight: bolder;">Acceler8 2nd Anniversary </p>
+		              			    <p class="place">Coworking community</p>
+		              			    <p class="place">November22,2017 6:00 PM </p>
+        				</div>
+        				
+      			</div>
+    			</div>
+    			</a>
+    			</div>
+    			<div class="col s12 m7">
+		<a href="#" style="color:black">
+				<div class="card horizontal" style="margin: 1.5rem 15px 3rem 12px;">
+      			<div class="card-image" style="max-width: 30%;">
+        				<img src="Img/Eventphoto-10.png" >
+      			</div>
+      			<div class="card-stacked">
+        				<div class="card-content">
+ 										 <p style="font-size: calc;font-weight: bolder;">Acceler8 2nd Anniversary </p>
+		              			    <p class="place">Coworking community</p>
+		              			    <p class="place">November22,2017 6:00 PM </p>
+        				</div>
+        				
+      			</div>
+    			</div>
+    			</a>
+    			</div>
+<div class="col s12 m7">
+		<a href="#" style="color:black">
+				<div class="card horizontal" style="margin: 1.5rem 15px 3rem 12px;">
+      			<div class="card-image" style="max-width: 30%;">
+        				<img src="Img/Eventphoto-10.png" >
+      			</div>
+      			<div class="card-stacked">
+        				<div class="card-content">
+ 										 <p style="font-size: calc;font-weight: bolder;">Acceler8 2nd Anniversary </p>
+		              			    <p class="place">Coworking community</p>
+		              			    <p class="place">November22,2017 6:00 PM </p>
+        				</div>
+        				
+      			</div>
+    			</div>
+    			</a>
+       </div>
+       </div>
+  <div id="test-swipe-2" class="col s12 ">	
+  <div class="col s12 m7">
+		<a href="#" style="color:black">
+				<div class="card horizontal" style="margin: 1.5rem 15px 3rem 12px;">
+      			<div class="card-image" style="max-width: 30%;">
+        				<img src="Img/Eventphoto-11.png" >
+      			</div>
+      			<div class="card-stacked">
+        				<div class="card-content">
+ 										 <p style="font-size: calc;font-weight: bolder;">Acceler8 2nd Anniversary </p>
+		              			    <p class="place">Coworking community</p>
+		              			    <p class="place">November22,2017 6:00 PM </p>
+        				</div>
+        				
+      			</div>
+    			</div>
+    			</a>
+ 			</div>
+ 			<div class="col s12 m7">
+		<a href="#" style="color:black">
+				<div class="card horizontal" style="margin: 1.5rem 15px 3rem 12px;">
+      			<div class="card-image" style="max-width: 30%;">
+        				<img src="Img/Eventphoto-10.png" >
+      			</div>
+      			<div class="card-stacked">
+        				<div class="card-content">
+ 										 <p style="font-size: calc;font-weight: bolder;">Acceler8 2nd Anniversary </p>
+		              			    <p class="place">Coworking community</p>
+		              			    <p class="place">November22,2017 6:00 PM </p>
+        				</div>
+        				
+      			</div>
+    			</div>
+    			</a>
+    			</div>
+<div class="col s12 m7">
+		<a href="#" style="color:black">
+				<div class="card horizontal" style="margin: 1.5rem 15px 3rem 12px;">
+      			<div class="card-image" style="max-width: 30%;">
+        				<img src="Img/Eventphoto-10.png" >
+      			</div>
+      			<div class="card-stacked">
+        				<div class="card-content">
+ 										 <p style="font-size: calc;font-weight: bolder;">Acceler8 2nd Anniversary </p>
+		              			    <p class="place">Coworking community</p>
+		              			    <p class="place">November22,2017 6:00 PM </p>
+        				</div>
+        				
+      			</div>
+    			</div>
+    			</a>
+    			</div>
+    			</div>
+  <div id="test-swipe-3" class="col s12">
+	<div class="col s12 m7">
+		<a href="#" style="color:black">
+				<div class="card horizontal" style="margin: 1.5rem 15px 3rem 12px;">
+      			<div class="card-image" style="max-width: 30%;">
+        				<img src="Img/Eventphoto-12.png" >
+      			</div>
+      			<div class="card-stacked">
+        				<div class="card-content">
+ 										 <p style="font-size: calc;font-weight: bolder;">Acceler8 2nd Anniversary </p>
+		              			    <p class="place">Coworking community</p>
+		              			    <p class="place">November22,2017 6:00 PM </p>
+        				</div>
+        				
+      			</div>
+    			</div>
+    			</a>
+ 			</div>
+ 			<div class="col s12 m7">
+		<a href="#" style="color:black">
+				<div class="card horizontal" style="margin: 1.5rem 15px 3rem 12px;">
+      			<div class="card-image" style="max-width: 30%;">
+        				<img src="Img/Eventphoto-10.png" >
+      			</div>
+      			<div class="card-stacked">
+        				<div class="card-content">
+ 										 <p style="font-size: calc;font-weight: bolder;">Acceler8 2nd Anniversary </p>
+		              			    <p class="place">Coworking community</p>
+		              			    <p class="place">November22,2017 6:00 PM </p>
+        				</div>
+        				
+      			</div>
+    			</div>
+    			</a>
+    			</div>
+
+<div class="col s12 m7">
+		<a href="#" style="color:black">
+				<div class="card horizontal" style="margin: 1.5rem 15px 3rem 12px;">
+      			<div class="card-image" style="max-width: 30%;">
+        				<img src="Img/Eventphoto-10.png" >
+      			</div>
+      			<div class="card-stacked">
+        				<div class="card-content">
+ 										 <p style="font-size: calc;font-weight: bolder;">Acceler8 2nd Anniversary </p>
+		              			    <p class="place">Coworking community</p>
+		              			    <p class="place">November22,2017 6:00 PM </p>
+        				</div>
+        				
+      			</div>
+    			</div>
+    			</a>
+    			</div>
+
+       </div>
+</div>
+    <div class="hide-on-med-and-down"> 
+
+   <section>
+         <div class="container container1">
+         <div class="col l12 dd" >
+         
+         
+         
+         
+     <ul id="tabs-swipe-demo" class="tabs" style="height: 58px;">
+    			<li class="tab col l3"  style="width: 25% !important;"><a  class="active" href="#test-swipe-11" style="color:white;">ALL</a></li>
+    			<li class="tab col l3" style="width: 25% !important;"><a   href="#test-swipe-21" style="color:white;">UPCOMING</a></li>
+    			<li class="tab col l3"  style="width: 25% !important;"><a  href="#test-swipe-31" style="color:white;">PREVIOUS</a></li>
+      </ul>
+         
+         
+         
+    </div>
+    <div class="row">
+    <div id="test-swipe-11" class="col s12">
+		<div>
+ 		<div class="col s12 m7 l4">
+			<a href="#" style="color:black">
+				<div class="card horizontal" style="margin: 1.5rem 15px 3rem 12px;">
+      			<div class="card-image" style="max-width: 30%;">
+        				<img src="Img/Eventphoto-10.png" >
+      			</div>
+      			<div class="card-stacked">
+        				<div class="card-content">
+ 										 <p style="font-size: calc;font-weight: bolder;">Acceler8 2nd Anniversary </p>
+		              			    <p class="place">Coworking community</p>
+		              			    <p class="place">November22,2017 6:00 PM </p>
+        				</div>
+        				
+      			</div>
+    			</div>
+    			</a>
+    	</div>
+    
+		</div>
+		<div>
+ 		<div class="col s12 m7 l4">
+			<a href="#" style="color:black">
+				<div class="card horizontal" style="margin: 1.5rem 15px 3rem 12px;">
+      			<div class="card-image" style="max-width: 30%;">
+        				<img src="Img/Eventphoto-10.png" >
+      			</div>
+      			<div class="card-stacked">
+        				<div class="card-content">
+ 										 <p style="font-size: calc;font-weight: bolder;">Acceler8 2nd Anniversary </p>
+		              			    <p class="place">Coworking community</p>
+		              			    <p class="place">November22,2017 6:00 PM </p>
+        				</div>
+        				
+      			</div>
+    			</div>
+    			</a>
+    	</div>
+    
+		</div>
+		<div>
+ 		<div class="col s12 m7 l4">
+			<a href="#" style="color:black">
+				<div class="card horizontal" style="margin: 1.5rem 15px 3rem 12px;">
+      			<div class="card-image" style="max-width: 30%;">
+        				<img src="Img/Eventphoto-10.png" >
+      			</div>
+      			<div class="card-stacked">
+        				<div class="card-content">
+ 										 <p style="font-size: calc;font-weight: bolder;">Acceler8 2nd Anniversary </p>
+		              			    <p class="place">Coworking community</p>
+		              			    <p class="place">November22,2017 6:00 PM </p>
+        				</div>
+        				
+      			</div>
+    			</div>
+    			</a>
+    	</div>
+    
+		</div>
+		<div>
+ 		<div class="col s12 m7 l4">
+			<a href="#" style="color:black">
+				<div class="card horizontal" style="margin: 1.5rem 15px 3rem 12px;">
+      			<div class="card-image" style="max-width: 30%;">
+        				<img src="Img/Eventphoto-10.png" >
+      			</div>
+      			<div class="card-stacked">
+        				<div class="card-content">
+ 										 <p style="font-size: calc;font-weight: bolder;">Acceler8 2nd Anniversary </p>
+		              			    <p class="place">Coworking community</p>
+		              			    <p class="place">November22,2017 6:00 PM </p>
+        				</div>
+        				
+      			</div>
+    			</div>
+    			</a>
+    	</div>
+    
+		</div>
+   
+  </div>
+  
+   
+  <div id="test-swipe-21" class="col s12">
+		<div>
+ 		<div class="col s12 m7 l4">
+			<a href="#" style="color:black">
+				<div class="card horizontal" style="margin: 1.5rem 15px 3rem 12px;">
+      			<div class="card-image" style="max-width: 30%;">
+        				<img src="Img/Eventphoto-11.png" >
+      			</div>
+      			<div class="card-stacked">
+        				<div class="card-content">
+ 										 <p style="font-size: calc;font-weight: bolder;">Acceler8 2nd Anniversary </p>
+		              			    <p class="place">Coworking community</p>
+		              			    <p class="place">November22,2017 6:00 PM </p>
+        				</div>
+        				
+      			</div>
+    			</div>
+    			</a>
+    	</div>
+    
+		</div>
+		<div>
+ 		<div class="col s12 m7 l4">
+			<a href="#" style="color:black">
+				<div class="card horizontal" style="margin: 1.5rem 15px 3rem 12px;">
+      			<div class="card-image" style="max-width: 30%;">
+        				<img src="Img/Eventphoto-10.png" >
+      			</div>
+      			<div class="card-stacked">
+        				<div class="card-content">
+ 										 <p style="font-size: calc;font-weight: bolder;">Acceler8 2nd Anniversary </p>
+		              			    <p class="place">Coworking community</p>
+		              			    <p class="place">November22,2017 6:00 PM </p>
+        				</div>
+        				
+      			</div>
+    			</div>
+    			</a>
+    	</div>
+    
+		</div>
+		<div>
+ 		<div class="col s12 m7 l4">
+			<a href="#" style="color:black">
+				<div class="card horizontal" style="margin: 1.5rem 15px 3rem 12px;">
+      			<div class="card-image" style="max-width: 30%;">
+        				<img src="Img/Eventphoto-10.png" >
+      			</div>
+      			<div class="card-stacked">
+        				<div class="card-content">
+ 										 <p style="font-size: calc;font-weight: bolder;">Acceler8 2nd Anniversary </p>
+		              			    <p class="place">Coworking community</p>
+		              			    <p class="place">November22,2017 6:00 PM </p>
+        				</div>
+        				
+      			</div>
+    			</div>
+    			</a>
+    	</div>
+    
+		</div>
+		<div>
+ 		<div class="col s12 m7 l4">
+			<a href="#" style="color:black">
+				<div class="card horizontal" style="margin: 1.5rem 15px 3rem 12px;">
+      			<div class="card-image" style="max-width: 30%;">
+        				<img src="Img/Eventphoto-10.png" >
+      			</div>
+      			<div class="card-stacked">
+        				<div class="card-content">
+ 										 <p style="font-size: calc;font-weight: bolder;">Acceler8 2nd Anniversary </p>
+		              			    <p class="place">Coworking community</p>
+		              			    <p class="place">November22,2017 6:00 PM </p>
+        				</div>
+        				
+      			</div>
+    			</div>
+    			</a>
+    	</div>
+    
+		</div>
+   
+  </div> 
+    <div id="test-swipe-31" class="col s12">
+    
+     		<div class="col s12 m7 l4">
+			<a href="#" style="color:black">
+				<div class="card horizontal" style="margin: 1.5rem 15px 3rem 12px;">
+      			<div class="card-image" style="max-width: 30%;">
+        				<img src="Img/Eventphoto-10.png" >
+      			</div>
+      			<div class="card-stacked">
+        				<div class="card-content">
+ 										 <p style="font-size: calc;font-weight: bolder;">Acceler8 2nd Anniversary </p>
+		              			    <p class="place">Coworking community</p>
+		              			    <p class="place">November22,2017 6:00 PM </p>
+        				</div>
+        				
+      			</div>
+    			</div>
+    			</a>
+    	</div>
+    	 		<div class="col s12 m7 l4">
+			<a href="#" style="color:black">
+				<div class="card horizontal" style="margin: 1.5rem 15px 3rem 12px;">
+      			<div class="card-image" style="max-width: 30%;">
+        				<img src="Img/Eventphoto-10.png" >
+      			</div>
+      			<div class="card-stacked">
+        				<div class="card-content">
+ 										 <p style="font-size: calc;font-weight: bolder;">Acceler8 2nd Anniversary </p>
+		              			    <p class="place">Coworking community</p>
+		              			    <p class="place">November22,2017 6:00 PM </p>
+        				</div>
+        				
+      			</div>
+    			</div>
+    			</a>
+    	</div>
+ 		<div class="col s12 m7 l4">
+			<a href="#" style="color:black">
+				<div class="card horizontal" style="margin: 1.5rem 15px 3rem 12px;">
+      			<div class="card-image" style="max-width: 30%;">
+        				<img src="Img/Eventphoto-10.png" >
+      			</div>
+      			<div class="card-stacked">
+        				<div class="card-content">
+ 										 <p style="font-size: calc;font-weight: bolder;">Acceler8 2nd Anniversary </p>
+		              			    <p class="place">Coworking community</p>
+		              			    <p class="place">November22,2017 6:00 PM </p>
+        				</div>
+        				
+      			</div>
+    			</div>
+    			</a>
+    	</div>
+    
+    
+    </div>
+  </div>
+  
+  
+  </div>
+</section>
+</div>
+<?php include "footer.html"; ?>
+<script type="text/javascript">
+$(document).ready(function () {
+	$("#logo").addClass('hide-on-med-and-down');
+	$(".search").show();
+
+    $('ul.tabs').tabs('select_tab', 'tab_id');
+
+});
+</script>
